6e9b3411a2cbab0492f3fcddfcb267a4b1c7dd14
WilsonChenault <chenaultwt@hendrix.edu>
2025-09-13 23:30:35 -0500

    Update circleScan.py
    progress on rewriting script to use no external files

--- circleScan.py
+++ circleScan.py
@@ -7,8 +7,17 @@ from time import time
 
 start = time()
 
+xfunc = np.linspace(0, 1, 100000)
 xPoints = np.linspace(0.64, 0.68, 1000) # x-points to be scanned
 print(xPoints) # DEBUG
+
+def curveA(x):
+    y = x**2
+    return y
+
+def curveB(x):
+    y = x**2 - 1
+    return y
 
 def pointMag(x1, y1, x2, y2):
     xMag = abs(x2 - x1)
@@ -16,59 +25,39 @@ def pointMag(x1, y1, x2, y2):
     magnitude = np.sqrt(xMag**2 + yMag**2)
     return magnitude
 
-def findLowestMag(fileA, fileB):
+def findLowestMag(fileA, fileB, index):
     counter = 0
+    smallestMag = 100
     with open(fileA, 'r') as file, open(fileB, 'w') as log:
         for line in file:
             line = line.split(', ')
             vectorMag = round(pointMag(float(line[0]), float(line[1]), xCenter, 0), 7)  # y2 always 0, centered on x-axis
             if vectorMag < smallestMagA:  # Keep smallest vectorMag that has been found;
                 smallestMagA = vectorMag
-            elif vectorMag == smallestMagA: # if equal no one cares
-                continue
-            elif vectorMag > smallestMagA: # Break loop if the magnitude ever increases
+            elif vectorMag >= smallestMagA: # Break loop if the magnitude ever increases
                 break
             counter += 1  # DEBUG
             log.write(str(vectorMag) + ', ' + str(counter) + ', ' + str(xCenter) + '\n')  # DEBUG
+    return smallestMag, counter
+
+# Write output of curves to array of y-values
+curveA = (x**2 for x in xfunc)
+curveB = (x**2 - 1 for x in xfunc)
+curveAOutput = np.fromiter(curveA, dtype = 'd')
+curveBOutput = np.fromiter(curveB, dtype = 'd')
 
 # Start scan to find the closest point on each curve; if they are equal, that might actually just be it...
-smallestMagA = 100 # Define smallest magnitude that is not real
-smallestMagB = 100 # Define smallest magnitude that is not real
-counterA = 0 # DEBUG
-counterB = 0 # DEBUG
-with (open('curveA.txt', 'r') as fileA, open('curveB.txt', 'r') as fileB,
-      open('magnitudesA.txt', 'w') as fileC, open('magnitudesB.txt', 'w') as fileD):
-    for xCenter in xPoints:
-        for line in fileA:
-            line = line.split(', ')
-            vectorMag = round(pointMag(float(line[0]), float(line[1]), xCenter, 0), 7) # y2 always 0, centered on x-axis
-            if vectorMag < smallestMagA: # Keep smallest vectorMag that has been found;
-                smallestMagA = vectorMag
-            elif vectorMag == smallestMagA:
-                continue
-            elif vectorMag > smallestMagA:
-                break
-            counterA += 1 # DEBUG
-            fileC.write(str(vectorMag) + ', ' + str(counterA) + ', ' + str(xCenter) + '\n') # DEBUG
-        for line in fileB:
-            line = line.split(', ')
-            vectorMag = round(pointMag(float(line[0]), float(line[1]), xCenter, 0), 7) # y2 always 0, centered on x-axis
-            if vectorMag < smallestMagB:
-                smallestMagB = vectorMag
-            elif vectorMag == smallestMagB:
-                continue
-            elif vectorMag > smallestMagB:
-                break
-            counterB += 1 # DEBUG
-            fileD.write(str(vectorMag) + ', ' + str(counterB) + ', ' + str(xCenter) + '\n') # DEBUG
-        if smallestMagA == smallestMagB:
-            break
-        else:
-            continue
-    fileA.close()
-    fileB.close()
-print(smallestMagA, counterA)
-print(smallestMagB, counterB)
+smallestMagA, smallestMagB = 100, 100 # Define smallest magnitude that is not real
+indexA, indexB = 0, 0 # Initial index
+for xCenter in xPoints:
+    smallestMagA, indexA = findLowestMag('curveA.txt', 'magnitudesA.txt', indexA)
+    smallestMagB, indexB = findLowestMag('curveB.txt', 'magnitudesB.txt', indexB)
+    if np.allclose(smallestMagA, smallestMagB):
+        break
+    else:
+        continue
+print(smallestMagA, indexA)
+print(smallestMagB, indexB)
 
 end = time()
 
